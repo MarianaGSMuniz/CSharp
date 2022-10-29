cff7087480632286c91c2ddb4fc48fe0b07c6f32
Mariana Muniz <maewerton60@gmail.com>
2022-10-29 13:34:53 -0300

    Variável tipo Double e Conversões

--- Program.cs
+++ Program.cs
@@ -1,2 +1,53 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program 
+{
+    static void Main(string[] args)
+    {   //variavel tipo int
+        Console.WriteLine("Criando Variavéis");
+
+        int idade;
+        idade = 31;
+
+        Console.WriteLine(idade);
+
+        idade = 2022 - 1991;
+
+        idade = 10 * 3 + 1;
+
+        idade = (10 - 40) * 1;
+
+        
+        // variavel tipo double
+
+        double salario;
+        salario = 3050.15;
+
+        Console.WriteLine(salario);
+
+
+        //Conversão e outros tipos numéricos
+
+        double salario2;
+        salario2 = 3000.15;
+
+        int salarioInteior;
+        salarioInteior = (int)salario2;
+
+        Console.WriteLine(salarioInteior);
+
+
+
+        Console.WriteLine("Tecle Enter para fechar");
+
+    }
+
+
+
+}
+
+
+
+
+
+
